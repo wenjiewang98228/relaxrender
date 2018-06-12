eb7127450a558d953b00f4541c7349bf168839c8
DragoonKiller <947426443@qq.com>
2018-06-12 11:15:07 +0800

    add unit test of blur. fix a bug.

--- features/depth_of_field/depth_of_field/blur.py
+++ features/depth_of_field/depth_of_field/blur.py
@@ -15,11 +15,10 @@ def blur(img,dimg,L1 = 1,L2 = 10):
 	rows,cols,a=img.shape
 	for k in range(rows):
 		for j in range(cols):
-			if(dimg[k,j,0] < L1 or dimg[k,j,0] > L2)
+			if dimg[k,j,0] < L1 or dimg[k,j,0] > L2 :
 				img[k,j]=blurimg[k,j]
 	
 	#plt.imshow(img)
 	#plt.show()
 	#misc.imsave('img.png',img)
 	return img
-
---
+++ features/depth_of_field/test.py
@@ -0,0 +1,2 @@
+# place holder recently.
+# may just collect and run all test py files.
