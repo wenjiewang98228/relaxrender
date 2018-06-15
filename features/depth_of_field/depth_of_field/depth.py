c9ff473d6e0ad44dbeef5ea341303359ecdb29f6
XinHuang98 <763479239@qq.com>
2018-06-15 11:45:12 +0800

    modify a parameter

--- features/depth_of_field/depth_of_field/depth.py
+++ features/depth_of_field/depth_of_field/depth.py
@@ -7,7 +7,9 @@ import numpy as np
 #row from 0~300 got less depth
 #row more than 300 got greater depth
 #but the change is too obvious
-def depth(path):
+
+
+def depth(path, high):
 	img = misc.imread(path)
 	dimg = misc.imread(path)
 	dimg = dimg[:, :, :, np.newaxis]
@@ -15,10 +17,10 @@ def depth(path):
 	add depth dimension
 	"""
 	rows, cols, a = img.shape
-	for k in range(0, 300, 1):
+	for k in range(0, high, 1):
 		for j in range(cols):
 			dimg[k, j, 0] = 5
-	for k in range(300, rows, 1):
+	for k in range(high, rows, 1):
 		for j in range(cols):
 			dimg[k, j, 0] = 20
 #	img2 = bl.blur(img, dimg)
