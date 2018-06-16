500d3f3635526986045aef815e3b5a035adda6d7
dnmnkdsj <1928088706@qq.com>
2018-06-17 00:45:52 +0800

    modifield DepthOfField.py(adjust usage of package)

--- features/depth_of_field/depth_of_field/DepthOfField.py
+++ features/depth_of_field/depth_of_field/DepthOfField.py
@@ -1,5 +1,6 @@
 import depth_of_field.blur  as bl
 import depth_of_field.depth as de
+import depth_of_field.blur_factor as blur_factor
 import matplotlib.pyplot as plt
 import sys
 import os
@@ -7,19 +8,23 @@ import os
 from sys import argv
 from scipy import misc
 
-#  get the picture path and use functions to work 
-#  the statements in the code is the function to complete  
+#  get the picture path and use functions to work
+#  the statements in the code is the function to complete
 def DepthOfField(i_path):
-	image_depth = de.depth(i_path[1],300)		
-	img = misc.imread(i_path[1])
+	dimg = de.depth(i_path[1])
+	fimg = blur_factor.blur_factor(dimg)
+	'''
+	dimg is image with depth axis
+	fimg is image with blur factor axis
+	'''
+	# image_depth = de.depth(i_path[1],300)
+	# img = misc.imread(i_path[1])
 #   factor = blf.blur_factor()
-	after = bl.blur(img,image_depth)
+	after = bl.blur(img,fimg)
 #  	after = bl.blur(img,image_depth,factor)
 	plt.imshow(after)
 	plt.show()
-	# show the picture after process 
+	# show the picture after process
 
 if __name__ == '__main__':
 	start(sys.argv)
-
-
