8e2d8edea0f24f40429199dac27e1ac1e590d8f8
ElderTrump <34269660+ElderTrump@users.noreply.github.com>
2018-06-24 23:35:25 +0900

    Update test_render.py

--- features/gamma_correction/test_render.py
+++ features/gamma_correction/test_render.py
@@ -3,9 +3,10 @@ from .render import *
 from skimage import data
 
 
-
+#测试类
 class Test(unittest.TestCase):
 
+#测试函数
     def test_gamma(self):    
         img = data.coffee()                  #测试图片
         gamma_correction(img,2.2,1,1,1)           #函数内调用我们的类 
