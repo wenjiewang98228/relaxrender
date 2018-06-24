6f8ab1e751a12b1a2cf7c32a5a9aaaf548c9c6ce
ElderTrump <34269660+ElderTrump@users.noreply.github.com>
2018-06-25 00:03:22 +0900

    Update test_render.py

--- features/gamma_correction/test_render.py
+++ features/gamma_correction/test_render.py
@@ -3,9 +3,15 @@ from .render import *
 from skimage import data
 
 
+#以下是对测试类的定义
 #测试类
 class Test(unittest.TestCase):
 
+    
+#下面编写测试函数
+#定义测试函数
+#包括导入测试图片
+#包括调用函数的类
 #测试函数
     def test_gamma(self):    
         img = data.coffee()                  #测试图片
